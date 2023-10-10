0c739a7b38fc709d374b826de59873c66ec76e28
ThlMrtns <thilo.martens@nerdware.dev>
2023-10-10 14:59:27 +0200

    feat: new endpoint

--- index.js
+++ index.js
@@ -14,4 +14,8 @@ app.get('/v3/', function (_req, res) {
   res.send('Hello V3 World')
 })
 
+app.get('/newfeature/', function (_req, res) {
+  res.send('New Feature Endpoint Response')
+})
+
 app.listen(3000)
